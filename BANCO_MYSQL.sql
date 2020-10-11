42a694214978310492c4a4c7830fb2c82abb6223
Matheus <matheussouzaferro@gmail.com>
2020-10-11 06:47:54 -0300

    Alteracoes no banco

--- BANCO_MYSQL.sql
+++ BANCO_MYSQL.sql
@@ -4,10 +4,25 @@ create table TB_CLIENTE(
 TB_CLIENTE_ID int auto_increment primary key  not null,
 TB_CLIENTE_NOME varchar(255) not null,
 TB_CLIENTE_SOBRENOME varchar(255) not null,
+TB_CLIENTE_CPF varchar(14) not null,
 TB_CLIENTE_EMAIL varchar(255) not null,
 TB_CLIENTE_CNH varchar(11) not null,
-TB_CLIENTE_DT_NASC varchar(20) /* VARCHAR PROVISORIO */
+TB_CLIENTE_DT_NASC date
 );
 
-
- select * from tb_cliente;
+create table TB_ENDERECO(
+TB_ENDERECO_ID int auto_increment primary key  not null,
+TB_ENDERECO_CEP varchar(9) not null,
+TB_ENDERECO_RUA varchar(255) not null,
+TB_ENDERECO_BAIRRO varchar(255) not null,
+TB_ENDERECO_COMPLEMENTO varchar(255) not null,
+TB_ENDERECO_CIDADE varchar(255) not null,
+TB_ENDERECO_ESTADO varchar(255) not null,
+TB_CLIENTE_ID int not null,
+constraint  CLIENTE_FK_ENDERECO_PK
+foreign key (TB_CLIENTE_ID)
+references TB_CLIENTE(TB_CLIENTE_ID)
+); 
+ 
+ select * from tb_cliente; 
+ select * from tb_ENDERECO; 
