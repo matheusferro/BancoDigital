927cd8e8c9827c49168c51a66857e2213a07ab65
Matheus <matheussouzaferro@gmail.com>
2020-10-11 07:00:04 -0300

    ADD. CPF E AJUSTE DTNASC

--- src/main/java/com/example/BancoDigital/model/Cliente.java
+++ src/main/java/com/example/BancoDigital/model/Cliente.java
@@ -1,13 +1,21 @@
 package com.example.BancoDigital.model;
 
-import com.example.BancoDigital.dto.ClienteDTO;
+import com.example.BancoDigital.dto.model.ClienteDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -25,6 +33,9 @@ public class Cliente {
     @Column(name = "TB_CLIENTE_SOBRENOME")
     private String sobrenome;
 
+    @Column(name = "TB_CLIENTE_CPF")
+    private String cpf;
+
     @Column(name = "TB_CLIENTE_EMAIL")
     private String email;
 
@@ -32,11 +43,15 @@ public class Cliente {
     private String cnh;
 
     @Column(name = "TB_CLIENTE_DT_NASC")
-    private String dtNasc;
+    @DateTimeFormat(pattern="YYYY-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate dtNasc;
 
-    public Cliente(String nome, String sobrenome, String email, String cnh, String dtNasc) {
+    public Cliente(String nome, String sobrenome, String cpf, String email, String cnh, LocalDate dtNasc) {
         this.nome = nome;
         this.sobrenome = sobrenome;
+        this.cpf = cpf;
         this.email = email;
         this.cnh = cnh;
         this.dtNasc = dtNasc;
