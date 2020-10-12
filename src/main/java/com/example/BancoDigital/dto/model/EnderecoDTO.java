af00d4933e86a7118394322cd9387d7a6265f0b0
Matheus <matheussouzaferro@gmail.com>
2020-10-12 00:29:46 -0300

    VALIDACOES MODEL

--- src/main/java/com/example/BancoDigital/dto/model/EnderecoDTO.java
+++ src/main/java/com/example/BancoDigital/dto/model/EnderecoDTO.java
@@ -7,6 +7,9 @@ import lombok.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
@@ -21,21 +24,28 @@ public class EnderecoDTO extends RepresentationModel<EnderecoDTO> {
     private Long clienteId;
 
     @NotNull
+    @NotBlank(message="CEP INVÁLIDO.")
+    @Pattern(regexp  = "\\d{5}-\\d{3}", message = "CEP NÃO ESTÁ NO FORMATO CORRETO.")
     private String cep;
 
     @NotNull
+    @NotBlank(message="RUA INVÁLIDO.")
     private String rua;
 
     @NotNull
+    @NotBlank(message="BAIRRO INVÁLIDO.")
     private String bairro;
 
     @NotNull
+    @NotBlank(message="COMPLEMENTO INVÁLIDO.")
     private String complemento;
 
     @NotNull
+    @NotBlank(message="CIDADE INVÁLIDO.")
     private String cidade;
 
     @NotNull
+    @NotBlank(message="ESTADO INVÁLIDO.")
     private String estado;
 
     public Endereco convertDTOToEntity(){ return new ModelMapper().map(this, Endereco.class);}
