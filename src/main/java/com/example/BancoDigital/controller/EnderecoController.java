0385cfec966323cebe661b83c0933368a9cf4dbc
Matheus <matheussouzaferro@gmail.com>
2020-10-12 00:30:41 -0300

    AJUSTADO CADASTRO DE ENDERECO DE CLIENTE

--- src/main/java/com/example/BancoDigital/controller/EnderecoController.java
+++ src/main/java/com/example/BancoDigital/controller/EnderecoController.java
@@ -1,10 +1,20 @@
 package com.example.BancoDigital.controller;
 
-import com.example.BancoDigital.model.Endereco;
+import com.example.BancoDigital.Utils.Validacao;
+import com.example.BancoDigital.dto.model.EnderecoDTO;
+import com.example.BancoDigital.dto.response.Response;
 import com.example.BancoDigital.service.endereco.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +25,20 @@ public class EnderecoController {
     private EnderecoService service;
 
     @PostMapping
-    public void cadastrarEndereco(@RequestBody Endereco _endereco){
-        service.cadastrarEndereco(_endereco);
+    public ResponseEntity<Response<EnderecoDTO>> cadastrarEndereco(@Valid @RequestBody EnderecoDTO _enderecodto, BindingResult errors){
+        Response<EnderecoDTO> response = new Response<>();
+
+        if (errors.hasErrors()) {
+            errors.getAllErrors().forEach(error -> response.addErrorMsgToResponse(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        _enderecodto = service.cadastrarEndereco(_enderecodto);
+        response.setData(_enderecodto);
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add(HttpHeaders.LOCATION, "link");
+        return new ResponseEntity<>(response, headers, HttpStatus.CREATED);
     }
 
     @GetMapping(path = "{id}")
--- src/main/java/com/example/BancoDigital/service/endereco/EnderecoService.java
+++ src/main/java/com/example/BancoDigital/service/endereco/EnderecoService.java
@@ -1,5 +1,6 @@
 package com.example.BancoDigital.service.endereco;
 
+import com.example.BancoDigital.dto.model.EnderecoDTO;
 import com.example.BancoDigital.model.Endereco;
 import com.example.BancoDigital.repository.endereco.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,14 @@ public class EnderecoService {
     @Autowired
     private EnderecoRepository repository;
 
-    public Endereco cadastrarEndereco(Endereco _endereco){
-        return repository.save(_endereco);
+    public EnderecoDTO cadastrarEndereco(EnderecoDTO _enderecodto){
+//        NÃO REQUISITADO VALIDAÇÃO
+//        Validacao validacao = new Validacao(this);
+//        if(validacao.enderecoDeClienteExistente(_enderecodto.getClienteId().toString())){
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um endereço cadastrado para esse cliente.");
+//        }
+        Endereco endereco =  repository.save(_enderecodto.convertDTOToEntity());
+        return endereco.convertEntityToDTO();
     }
 
     public List<Map<String, String>> buscarEnderecoPorCliente(String _clienteId){
