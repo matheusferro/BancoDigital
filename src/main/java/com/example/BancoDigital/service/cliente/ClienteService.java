b9ae323a6374507de37c9e52d72b82c3d557483e
Matheus <matheussouzaferro@gmail.com>
2020-10-12 22:45:10 -0300

    TRATAMENTO DE ERRO

--- src/main/java/com/example/BancoDigital/service/cliente/ClienteService.java
+++ src/main/java/com/example/BancoDigital/service/cliente/ClienteService.java
@@ -26,26 +26,45 @@ public class ClienteService implements ClienteInterface {
     private ClienteRepository repository;
 
     public ClienteDTO cadastroCliente(ClienteDTO _clientedto){
-        Validacao validacao = new Validacao(this);
-        if(!validacao.dtNascMaior18(_clientedto.getDtNasc())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Necessário ser maior de idade.");
+        try {
+            Validacao validacao = new Validacao(this);
+            if (!validacao.dtNascMaior18(_clientedto.getDtNasc())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Necessário ser maior de idade.");
+            }
+            if (validacao.emailExistente(_clientedto.getEmail())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email existente.");
+            }
+            if (validacao.cpfExistente(_clientedto.getCpf())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CPF existente.");
+            }
+            Cliente cliente = repository.save(_clientedto.convertDTOToEntity());
+
+            return cliente.convertEntityToDTO();
+        }catch(Exception error){
+            error.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu um erro inesperado. Tente novamente.");
         }
-        if(validacao.emailExistente(_clientedto.getEmail())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email existente.");
-        }
-        if(validacao.cpfExistente(_clientedto.getCpf())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CPF existente.");
-        }
-        Cliente cliente = repository.save(_clientedto.convertDTOToEntity());
-        
-        return cliente.convertEntityToDTO();
     }
 
     public List<Cliente> listaTodosClientes(){
         return repository.findAll();
     }
 
-    public List<Map<String, String>> listaClientePorCpf(String _cpf){ return repository.findClienteByCpf(_cpf); }
+    public List<Map<String, String>> listaClientePorCpf(String _cpf){
+        try {
+            return repository.findClienteByCpf(_cpf);
+        }catch(Exception error){
+            error.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu um erro inesperado. Tente novamente.");
+        }
+    }
 
-    public List<Map<String, String>> listaClientePorEmail(String _email){ return repository.findClienteByEmail(_email); }
+    public List<Map<String, String>> listaClientePorEmail(String _email){
+        try{
+            return repository.findClienteByEmail(_email);
+        }catch(Exception error){
+            error.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu um erro inesperado. Tente novamente.");
+        }
+    }
 }
--- src/main/java/com/example/BancoDigital/service/endereco/EnderecoService.java
+++ src/main/java/com/example/BancoDigital/service/endereco/EnderecoService.java
@@ -4,7 +4,9 @@ import com.example.BancoDigital.dto.model.EnderecoDTO;
 import com.example.BancoDigital.model.Endereco;
 import com.example.BancoDigital.repository.endereco.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -16,16 +18,26 @@ public class EnderecoService {
     private EnderecoRepository repository;
 
     public EnderecoDTO cadastrarEndereco(EnderecoDTO _enderecodto){
+        try {
 //        NÃO REQUISITADO VALIDAÇÃO
 //        Validacao validacao = new Validacao(this);
 //        if(validacao.enderecoDeClienteExistente(_enderecodto.getClienteId().toString())){
 //            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um endereço cadastrado para esse cliente.");
 //        }
-        Endereco endereco =  repository.save(_enderecodto.convertDTOToEntity());
-        return endereco.convertEntityToDTO();
+            Endereco endereco = repository.save(_enderecodto.convertDTOToEntity());
+            return endereco.convertEntityToDTO();
+        }catch(Exception error){
+            error.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu um erro inesperado. Tente novamente.");
+        }
     }
 
     public List<Map<String, String>> buscarEnderecoPorCliente(String _clienteId){
-        return repository.findByTbClienteId(_clienteId);
+        try {
+            return repository.findByTbClienteId(_clienteId);
+        }catch(Exception error){
+            error.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu um erro inesperado. Tente novamente.");
+        }
     }
 }
